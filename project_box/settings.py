2047a8cd8353f6a86a93f490ca145b1c2ad0623b
dev-jey <savalijey@gmail.com>
2021-04-24 12:07:32 +0300

    Fix gunicorn

--- project_box/settings.py
+++ project_box/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv(
+    "SECRET_KEY", "kienasusnduowndwjkn@#4njsbnuixbuasi#@$@#$@#$@#$#@$nduibuycbyuwevci3wdi823@@#4icg878432brduibw@##@@##@#@dwqd!@@Edwdqw32#EQW")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
